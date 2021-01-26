1730d39a9deb17b8e01063a9c365ea7227204557
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-01-26 17:21:44 +0900

    working like old

--- bezkoder/src/main/java/mas/bezkoder/controller/Parser.java
+++ bezkoder/src/main/java/mas/bezkoder/controller/Parser.java
@@ -3,12 +3,29 @@ package mas.bezkoder.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
     public static String parseHtml(String input) {
-//        var a_pattern = /<a [^>]*href="[^"]*"[^>]*>/gm
+        String href = "href=\"([^\"]*)\"";
+        String src = "src=\"([^\"]*)\"";
+        Pattern pattern = Pattern.compile(src);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            input = input.replaceAll(group, "http://localhost:8082/" + group);
+        }
+        pattern = Pattern.compile(href);
+        matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            input = input.replaceAll(group, "http://localhost:8082/" + group);
+        }
+
         return input;
     }
 
