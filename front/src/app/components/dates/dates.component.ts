e79db3885fe7b9bf67d7c37457971214dddd7288
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-12 14:28:16 +0900

    debug

--- front/src/app/components/dates/dates.component.ts
+++ front/src/app/components/dates/dates.component.ts
@@ -33,6 +33,8 @@ export class DatesComponent implements OnInit {
   }
 
   getDate1(): string {
+    if (this.date1 == null) console.log("HELOPPPPPPPP")
+    else console.log(this.date1)
     var url = "http://118.67.133.84:8085/api/comparison?web=http://crdclub4wraumez4.onion/&prev=" +
       this.date1.replace(/-/g,"");
     return <string>this.sanitizer.bypassSecurityTrustHtml(url);
