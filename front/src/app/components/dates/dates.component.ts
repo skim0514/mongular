1cd54ed263d4f29cee8e9a50cc06d6ace8778326
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-12 13:08:07 +0900

    pls

--- front/src/app/components/dates/dates.component.ts
+++ front/src/app/components/dates/dates.component.ts
@@ -53,14 +53,15 @@ export class DatesComponent implements OnInit {
   getChanges(): void {
     if(this.date1 == null) return;
 
-    this.setDate1(this.date1);
-    this.activated = true;
-    this.double = false;
     this.websitesService.getChanges(this.website, this.date1).subscribe(
       data => {
         this.content1 = data as string;
         console.log(data);
       })
+
+    this.setDate1(this.date1);
+    this.activated = true;
+    this.double = false;
   }
 
   getComparison(): void {
