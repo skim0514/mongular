5828f4d0183f78c012115a24655c07281d2c3001
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-12 14:08:27 +0900

    try again

--- front/src/app/components/dates/dates.component.ts
+++ front/src/app/components/dates/dates.component.ts
@@ -50,20 +50,10 @@ export class DatesComponent implements OnInit {
     )
   }
 
-  getWebsite(): void {
-    this.websitesService.getChanges(this.website, this.date1).subscribe(
-      data => {
-        this.content1 = data;
-        console.log(data);
-      })
-  }
 
   getChanges(): void {
     if(this.date1 == null) return;
     this.setDate1(this.date1);
-    this.getWebsite();
-
-
     this.activated = true;
     this.double = false;
   }
