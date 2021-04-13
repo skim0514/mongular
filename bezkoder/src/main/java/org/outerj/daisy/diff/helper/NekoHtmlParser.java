6ea27fe519355836fe86734dc1dda241b986a6ae
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-13 19:44:54 +0900

    plssss

--- bezkoder/src/main/java/org/outerj/daisy/diff/helper/NekoHtmlParser.java
+++ bezkoder/src/main/java/org/outerj/daisy/diff/helper/NekoHtmlParser.java
@@ -15,7 +15,8 @@
  */
 package org.outerj.daisy.diff.helper;
 
-import org.cyberneko.html.parsers.SAXParser;
+import org.apache.xerces.parsers.SAXParser;
+
 import org.xml.sax.*;
 import org.xml.sax.helpers.AttributesImpl;
 
