7f4549a1d52d8ce083915b8ddc989fbb8bea385b
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-23 10:59:44 +0900

    try with prehandle

--- bezkoder/src/main/java/mas/bezkoder/controller/WildCardController.java
+++ bezkoder/src/main/java/mas/bezkoder/controller/WildCardController.java
@@ -47,49 +47,4 @@ public class WildCardController implements WebMvcConfigurer {
         registry.addInterceptor(new CustomInterceptor());
     }
 
-
-    @GetMapping("**")
-    public static ResponseEntity<?> getRandom(HttpServletRequest request) throws URISyntaxException, IOException {
-        System.out.println("random");
-        String requestURL = request.getRequestURI();
-        String referer = request.getHeader(HttpHeaders.REFERER);
-        final List<String> headerNames = Collections.list(request.getHeaderNames());
-        if (requestURL.startsWith("/api")) requestURL = requestURL.replace("/api/", "");
-        if (referer == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        System.out.println("getReferer");
-        URI url = new URI(referer);
-        List<NameValuePair> params = URLEncodedUtils.parse(url, StandardCharsets.UTF_8);
-        String base = null;
-        String date = null;
-        for (NameValuePair param : params) {
-            if (param.getName().equals("date")) date = param.getValue();
-            if (param.getName().equals("web")) base = param.getValue();
-        }
-        if (base == null || date == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        String link = HTMLExtractor.replaceUrl(requestURL, base);
-        if (link == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        String requestString = "http://localhost:8085/api/websites?date=" + date + "&web=" + java.net.URLEncoder.encode(link, StandardCharsets.UTF_8.name());
-        System.out.println("getURL " + requestString);
-        HttpGet httpGet = new HttpGet(requestString);
-        for (String headerName : headerNames) {
-            if (headerName.equals(HttpHeaders.REFERER)) httpGet.addHeader(HttpHeaders.REFERER, referer);
-            else httpGet.addHeader(headerName, request.getHeader(headerName));
-        }
-        CloseableHttpClient client = HttpClients.createDefault();
-        System.out.println("client");
-        CloseableHttpResponse response = client.execute(httpGet);
-        System.out.println("response");
-        HttpEntity entity = response.getEntity();
-        if (entity == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);;
-        HttpHeaders httpheaders = new HttpHeaders();
-        for(Header header : response.getAllHeaders()) {
-            httpheaders.add(header.getName(), header.getValue());
-        }
-        System.out.println("entity");
-        String result = EntityUtils.toString(entity);
-        StatusLine sl = response.getStatusLine();
-        if (sl.getStatusCode() > 299) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        client.close();
-        return new ResponseEntity<>(result, httpheaders, HttpStatus.OK);
-    }
 }
