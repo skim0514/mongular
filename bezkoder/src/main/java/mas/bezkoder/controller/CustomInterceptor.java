fc7676fc1ee99315af47f2d2fd81e14e8c265a06
Sean Kim <skim@Seans-MacBook-Pro.local>
2021-04-23 12:45:54 +0900

    new wookring handler

--- bezkoder/src/main/java/mas/bezkoder/controller/CustomInterceptor.java
+++ bezkoder/src/main/java/mas/bezkoder/controller/CustomInterceptor.java
@@ -28,28 +28,49 @@ import java.util.List;
 public class CustomInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("start prehandle");
         String referer = request.getHeader(HttpHeaders.REFERER);
+        String requestURI = request.getRequestURI();
         System.out.println("referer is " + referer);
-        if (referer == null)
-            System.out.println("referer null");
-        System.out.println("request is " + request.getRequestURI());
+        if (referer.contains("http://118.67.133.84:8085/api/websites")) {
+            if (requestURI.equals("/api/websites")) return true;
+            else {
+                CloseableHttpResponse proxyResponse = getRandom(request);
+                if (proxyResponse == null) {
+                    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                    return false;
+                }
+                System.out.println("response");
+                HttpEntity entity = proxyResponse.getEntity();
+                if (entity == null) {
+                    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                    return false;
+                }
+                for (Header header : proxyResponse.getAllHeaders()) {
+                    response.setHeader(header.getName(), header.getValue());
+                }
+                System.out.println("entity");
+                String result = EntityUtils.toString(entity);
+                StatusLine sl = proxyResponse.getStatusLine();
+                response.setStatus(sl.getStatusCode());
+                response.getWriter().write(result);
+                response.getWriter().flush();
+                return false;
+            }
+        }
         return true;
     }
 
 
 
-    public static ResponseEntity<?> getRandom(HttpServletRequest request) throws URISyntaxException, IOException {
+    public static CloseableHttpResponse getRandom(HttpServletRequest request) throws URISyntaxException, IOException {
         System.out.println("random");
         String requestURL = request.getRequestURI();
         String referer = request.getHeader(HttpHeaders.REFERER);
         final List<String> headerNames = Collections.list(request.getHeaderNames());
         if (requestURL.startsWith("/api")) requestURL = requestURL.replace("/api/", "");
-        if (referer == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        if (referer == null) return null;
         System.out.println("getReferer");
         URI url = new URI(referer);
         List<NameValuePair> params = URLEncodedUtils.parse(url, StandardCharsets.UTF_8);
@@ -59,9 +80,9 @@ public class CustomInterceptor implements HandlerInterceptor {
             if (param.getName().equals("date")) date = param.getValue();
             if (param.getName().equals("web")) base = param.getValue();
         }
-        if (base == null || date == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        if (base == null || date == null) return null;
         String link = HTMLExtractor.replaceUrl(requestURL, base);
-        if (link == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        if (link == null) return null;
         String requestString = "http://localhost:8085/api/websites?date=" + date + "&web=" + java.net.URLEncoder.encode(link, StandardCharsets.UTF_8.name());
         System.out.println("getURL " + requestString);
         HttpGet httpGet = new HttpGet(requestString);
@@ -72,18 +93,7 @@ public class CustomInterceptor implements HandlerInterceptor {
         CloseableHttpClient client = HttpClients.createDefault();
         System.out.println("client");
         CloseableHttpResponse response = client.execute(httpGet);
-        System.out.println("response");
-        HttpEntity entity = response.getEntity();
-        if (entity == null) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);;
-        HttpHeaders httpheaders = new HttpHeaders();
-        for(Header header : response.getAllHeaders()) {
-            httpheaders.add(header.getName(), header.getValue());
-        }
-        System.out.println("entity");
-        String result = EntityUtils.toString(entity);
-        StatusLine sl = response.getStatusLine();
-        if (sl.getStatusCode() > 299) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         client.close();
-        return new ResponseEntity<>(result, httpheaders, HttpStatus.OK);
+        return response;
     }
 }
